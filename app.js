6eca3a2e08260905fb1d09bd4484736b5e010466
YOUR FULL NAME <justindavenport@Justins-MacBook-Pro.local>
2017-05-13 16:04:46 -0700

    fixed #3

--- app.js
+++ app.js
@@ -51,11 +51,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
+  var dubSum = sum(sum(a,b)[0], c)[0];
+  var dubProduct = multiply(multiply(a,b)[0], c)[0];
+  var mySumString = a + ' and ' + b + ' and ' + c + ' sum to ' + dubSum + '.';
+  var myProductString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + dubProduct +'.';
+  return [dubSum, dubProduct, mySumString, myProductString];
 
 }
 
+sumAndMultiply();
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -69,13 +75,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 
-function sumArray(testArray){ //eslint-disable-line
+function sumArray(testArray){
+  //eslint-disable-line
+
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ //testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
